49cf2cbdc35955a36f12fe12d5a6130382c815a7
LujunW <LujunWeng@outlook.com>
2018-08-07 00:03:13 +0930

    Add another selection method

---
+++ src/evo/ParentSelectionOpImp/RouletteWheelSelection.java
@@ -0,0 +1,17 @@
+package evo.ParentSelectionOpImp;
+
+import evo.ParentSelectionOp;
+import evo.core.Individual;
+import evo.core.Population;
+
+import java.util.List;
+
+/**
+ * Created by LujunW on 2018/8/7.
+ */
+public class RouletteWheelSelection implements ParentSelectionOp {
+    @Override
+    public List<Individual> apply(Population p) {
+        return null;
+    }
+}
