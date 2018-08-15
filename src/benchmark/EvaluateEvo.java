bbccc6925a07b31bc78aa66385d9c692ec6e8eef
LujunW <LujunWeng@outlook.com>
2018-08-15 21:39:01 +0930

    generate results

--- src/benchmark/EvaluateEvo.java
+++ src/benchmark/EvaluateEvo.java
@@ -10,29 +10,47 @@ import evo.SuvivorSelectionOpImp.ElitismSuvivorSelection;
 import evo.core.*;
 import tspproblem.TSPProblem;
 
+/**
+ * This class evaluate evolutionary algorithms as required in the assignment handout.
+ *
+ * 1. Design and test three evolutionary algorithms on all required instances,
+ *    with population size 20, 50, 100, 200. Report the outcomes after 2000,
+ *    5000, 10000 and 20000 generations
+ * 2. Evaluate the best of the three with a population size of 50 for 20000 generations.
+ *    Report mean, standard deviation for running 30 times on each instances.
+ */
 public class EvaluateEvo {
     private static class SimpleReporter implements Reporter {
 
         private final String algoName;
         private final String fn;
         private final int popSize;
+        private final boolean onlyLast;
 
-        public SimpleReporter(String algo, String fn, int popSize) {
+        public SimpleReporter(String algo, String fn, int popSize, boolean onlyLast) {
             this.algoName = algo;
             this.fn = fn;
             this.popSize = popSize;
+            this.onlyLast = onlyLast;
         }
 
         @Override
         public void apply(int gen, Population p) {
-            switch (gen) {
-//                case 0: case 2000:
-//                case 5000: case 10000:
-                case 20000:
+            if (!this.onlyLast) {
+                switch (gen) {
+                    case 0:
+                    case 2000:
+                    case 5000:
+                    case 10000:
+                    case 20000:
+                        report(gen, p.getBestFitnessValue());
+                        break;
+                    default:
+                        break;
+                }
+            } else {
+                if (gen == 20000)
                     report(gen, p.getBestFitnessValue());
-                    break;
-                default:
-                    break;
             }
         }
 
@@ -55,7 +73,27 @@ public class EvaluateEvo {
         CrossOverOp coOp = new OrderCrossOver(1.0);
         MutationOp muOp = new InversionMutation(1.0);
         SurvivorSelectionOp sSelOp = new ElitismSuvivorSelection();
-        Reporter reporter = new SimpleReporter("TOI", fn, popSize);
+        Reporter reporter = new SimpleReporter("TOI", fn, popSize, false);
+
+        TSPSolver solver = EvoSolverBuilder.buildGeneric(
+                popSize,
+                genNum,
+                pSelOp,
+                coOp,
+                muOp,
+                sSelOp,
+                reporter
+        );
+        solver.solve(inst);
+    }
+
+    public static void TOIReportLast(int popSize, int genNum, String fn) {
+        TSPProblem inst = new TSPProblem(fn);
+        ParentSelectionOp pSelOp = new TournamentSelection(2, (int)(popSize/3));
+        CrossOverOp coOp = new OrderCrossOver(1.0);
+        MutationOp muOp = new InversionMutation(1.0);
+        SurvivorSelectionOp sSelOp = new ElitismSuvivorSelection();
+        Reporter reporter = new SimpleReporter("TOI", fn, popSize, true);
 
         TSPSolver solver = EvoSolverBuilder.buildGeneric(
                 popSize,
@@ -75,7 +113,7 @@ public class EvaluateEvo {
         CrossOverOp coOp = new OrderCrossOver(1.0);
         MutationOp muOp = new SwapMutation(1.0);
         SurvivorSelectionOp sSelOp = new ElitismSuvivorSelection();
-        Reporter reporter = new SimpleReporter("TOS", fn, popSize);
+        Reporter reporter = new SimpleReporter("TOS", fn, popSize, false);
 
         TSPSolver solver = EvoSolverBuilder.buildGeneric(
                 popSize,
@@ -95,7 +133,7 @@ public class EvaluateEvo {
         CrossOverOp coOp = new OrderCrossOver(1.0);
         MutationOp muOp = new InversionMutation(1.0);
         SurvivorSelectionOp sSelOp = new ElitismSuvivorSelection();
-        Reporter reporter = new SimpleReporter("ROI", fn, popSize);
+        Reporter reporter = new SimpleReporter("ROI", fn, popSize, false);
 
         TSPSolver solver = EvoSolverBuilder.buildGeneric(
                 popSize,
@@ -110,19 +148,34 @@ public class EvaluateEvo {
     }
 
     public static void main(String[] args) {
+        System.out.println("# Test three evolutionary algorithms and benchmark the best");
+        System.out.println("# ROI: RouletteWheel,\tOrderCrossOver(p=1.0),\tInversion Mutation(p=1.0)");
+        System.out.println("# TOI: Tournament(k=popSize/3),\tOrderCrossOver(p=1.0),\tInversion Mutation(p=1.0)");
+        System.out.println("# TOS: Tournament(k=popSize/3),\tOrderCrossOver(p=1.0),\tSwap Mutation(p=1.0");
+        System.out.println("# Notes: Results for PR2392 and USA13509 will come at the end because of long time of running.");
+        System.out.println("# Notes: Results are in CSV format");
+        System.out.println();
         quickTest();
-        // slowTest();
-        // benchmarkTOI();
+        quickBenchmark();
+
+        System.out.println();
+        System.out.println("# The following running might take a long time");
+        slowTest();
+        slowBenchmark();
+
     }
 
     private static void slowTest() {
+        System.out.println("# Compare three algorithms with varying population sizes");
         System.out.println(
                 "AlgoName,FileName,PopSize,Gen,Cost"
         );
         testEvo("testfiles/pr2392.tsp");
         testEvo("testfiles/usa13509.tsp");
     }
+
     private static void quickTest() {
+        System.out.println("# Compare three algorithms with varying population sizes");
         System.out.println(
                 "AlgoName,FileName,PopSize,Gen,Cost"
         );
@@ -137,7 +190,8 @@ public class EvaluateEvo {
         testEvo("testfiles/pcb442.tsp");
     }
 
-    public static void benchmarkTOI() {
+    public static void quickBenchmark() {
+        System.out.println("# Benchmark the best algorithm of the three");
         System.out.println(
                 "AlgoName,FileName,PopSize,Gen,Cost"
         );
@@ -151,14 +205,30 @@ public class EvaluateEvo {
                 "testfiles/kroD100.tsp",
                 "testfiles/lin105.tsp",
                 "testfiles/pcb442.tsp",
-                "testfiles/pr2392.tsp"
         };
         for (String fn : files) {
             for (int i = 0; i < 30; i++) {
-                TOI(50, 20000, fn);
+                TOIReportLast(50, 20000, fn);
             }
         }
     }
+
+    public static void slowBenchmark() {
+        System.out.println("# Benchmark the best algorithm of the three");
+        System.out.println(
+                "AlgoName,FileName,PopSize,Gen,Cost"
+        );
+        String[] files = new String[]{
+                "testfiles/pr2392.tsp",
+                "testfiles/usa13509.tsp"
+        };
+        for (String fn : files) {
+            for (int i = 0; i < 30; i++) {
+                TOIReportLast(50, 20000, fn);
+            }
+        }
+    }
+
     public static void testEvo(String fn) {
         int[] popSizes = new int[]{20, 50, 100, 200};
         int gen = 20000;
